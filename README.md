959328e387b9e4dde23c977cd28136a08b4d00f2
sangam <thasalsangam02@gmail.com>
2022-07-26 17:19:54 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# flaskHandon
---
+++ flaskproject/demo.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/regForm")
+def RegForm():
+    return render_template('regForm.html')
+
+@app.route("/profile")
+def Profile():
+    return render_template('profile.html')
+
+@app.route("/about")
+def About():
+    return render_template('About.html')
+
+@app.route("/todo")
+def todo():
+    return render_template('todo.html')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
